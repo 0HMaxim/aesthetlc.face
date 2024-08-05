4f7fe3040b9cd6f9d4fde458d23b76f0245f6092
0HMaxim <teran5656@gmail.com>
2024-08-06 01:19:27 +0200

    added slider and advantages boxes

--- js/index.js
+++ js/index.js
@@ -1123,4 +1123,32 @@ if(currentFile=='gallery.html'){
             
                                 
                                 
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.angebot_item');
+    const totalSlides = slides.length;
+  
+    function showSlide(index) {
+      if (index >= totalSlides) {
+        currentSlide = 0;
+      } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+      } else {
+        currentSlide = index;
+      }
+  
+      const offset = -currentSlide * 100;
+      document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+    }
+  
+    function nextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function prevSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    document.querySelector('.next').addEventListener('click', nextSlide);
+    document.querySelector('.prev').addEventListener('click', prevSlide);
+  });
